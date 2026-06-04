2466e18ff2ee999f78739d1ca2090a43a5e91ef5
oluekele <ekeleolu1@gmail.com>
2026-06-04 14:29:45 +0100

    database dist resolved

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,21 +1,8 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    const connectionString = process.env.DATABASE_URL;
-
-    if (!connectionString) {
-      throw new Error('DATABASE_URL is required to start Prisma.');
-    }
-
-    super({
-      adapter: new PrismaPg(connectionString),
-    });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
